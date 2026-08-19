1247b175a32f8178d6535e045b8160dcd6f1d9d9
edris.a@superblocks.clark <edris.a@superblocks.clark>
2026-08-19 14:33:36 -0400

    first message

---
+++ backends/cvent123/step1.py
@@ -0,0 +1,9 @@
+# Use Python to:
+#
+# 1. Transform the output of previous Steps by referencing their output (ex. Step1.output)
+# 2. Write logic referencing UI components (ex. Table1.selectedRow.id)
+# 3. Make http calls using requests (import requests)
+#
+# More libraries are coming in the future. Superblocks uses Python 3.
+
+return {"value": "Hello Superblocks!"}
